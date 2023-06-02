2c63ae2e1fdd936b1ad2a29c55c0741fa6cc9fc4
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-02 10:46:58 -0500

    edits in previous file

--- 05-30-2023/index.js
+++ 05-30-2023/index.js
@@ -1,77 +1,89 @@
-var example1="123";
-var example2=123;
+// var example1="123";
+// var example2=123;
 
-var addition=example1+example2;
+// var addition=example1+example2;
 
-console.log(addition);
+// console.log(addition);
 
 
-if(example1===example2){
-    console.log(true);
+// if(example1===example2){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// let x="Hi";
+
+
+
+// function new1(){
+//     let x="Hello";
+//         console.log(x);
+// }
+// new1();
+// console.log(x);
+
+// var car={
+//     type: {small: "sedan", big: "suv", medium: "jeep"},
+//     model:"v4",
+//     color:"black",
+//     fuelType:"gas",
+// }
+
+// car.type.medium="truck";
+
+// console.log(car.type.medium);
+
+// const arr=[1,2,3,4];
+
+// arr.push(5);
+// arr.push(6);
+
+// console.log(arr);
+
+
+// var str="welcome";
+// var str1="the";
+
+
+// var batch=`Hello, ${str} to ${str1} class`;
+// console.log(batch);
+
+// var numberList={}
+
+// var alphabets=["a","b","c","d","e","f","g","h"];
+
+// for(var i=0;i<alphabets.length;i++){
+//    console.log(alphabets[i]+ ":" + i);
+// }
+
+// /*
+// create a object about your profile  
+// First name,last name, DOB, hobbies should array with object sport type, acheivements, ,
+
+
+// display last hobbies in console.log and your display avatar or image in the page.
+
+
+// with height 250px and width 250px and cricle image
+
+// with minimum 10 fields 
+
+// own photo in assets folder
+
+
+
+// */
+
+function add(x,y){
+var z=10;
+return app(x,y,z);
+ 
 }
-else{
-    console.log(false);
+
+function app(m,k,l){
+    return m+k+l;
 }
 
-let x="Hi";
-
-
-
-function new1(){
-    let x="Hello";
-        console.log(x);
-}
-new1();
-console.log(x);
-
-var car={
-    type: {small: "sedan", big: "suv", medium: "jeep"},
-    model:"v4",
-    color:"black",
-    fuelType:"gas",
-}
-
-car.type.medium="truck";
-
-console.log(car.type.medium);
-
-const arr=[1,2,3,4];
-
-arr.push(5);
-arr.push(6);
-
-console.log(arr);
-
-
-var str="welcome";
-var str1="the";
-
-
-var batch=`Hello, ${str} to ${str1} class`;
-console.log(batch);
-
-var numberList={}
-
-var alphabets=["a","b","c","d","e","f","g","h"];
-
-for(var i=0;i<alphabets.length;i++){
-   console.log(alphabets[i]+ ":" + i);
-}
-
-/*
-create a object about your profile  
-First name,last name, DOB, hobbies should array with object sport type, acheivements, ,
-
-
-display last hobbies in console.log and your display avatar or image in the page.
-
-
-with height 250px and width 250px and cricle image
-
-with minimum 10 fields 
-
-own photo in assets folder
-
-
-
-*/
+console.log(add(5,6));
