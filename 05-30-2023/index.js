27706ba598924d6710db1a3540dbb9a30f434f1b
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-08 18:23:30 -0500

    new assignment added

--- 05-30-2023/index.js
+++ 05-30-2023/index.js
@@ -1,21 +1,27 @@
-let akshay =() => {
-    console.log("akshay");
+Array.prototype.sortMethod = function(){
+//  var c= this[1];
+//return c;
+        var newArr=[];
+        for (var i = 0; i < this.length; i++)   
+           {  
+             for (var j = i + 1; j < this.length; j++)   
+             {  
+              var tmp = 0;  
+              if (this[i] > this[j])   
+              {  
+                tmp = this[i];  
+                this[i] = this[j];  
+                this[j] = tmp;  
+}  
+} 
+newArr.push(this[i]);  
 }
+        return newArr[1];
+    }
+   
+myArray = [11,234,1,89,34,546,32]
+// myArray.sort();
+console.log(myArray.sortMethod());
 
-akshay();
-
-
-
-//adding two numbers
-
-let ajay =(a,b) =>{
-    return a + b;
-}
-
-console.log(ajay(2,4));
-
-
-let arrayValues=["hello","where","are","you"];
-
-console.log(arrayValues.toString());
+//
 
